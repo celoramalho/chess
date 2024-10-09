55a95c04b7cdfe8f1407187dbade040cb4739db6
anubis_greyignis <marceloramalho.info@gmail.com>
2024-10-09 19:02:27 -0300

    Thinking in chess math logic

---
+++ board.py
@@ -0,0 +1,17 @@
+class Board:
+
+    def __init__(self):
+        self.board = ["*"*8]*8
+
+    def create():
+        board = []
+        board.append(["*"*8]*8)
+        return board[0]
+
+    def show(board):
+        print(*board, sep="\n")
+
+    
+
+
+    #def initiate_game():
---
+++ piece.py
@@ -0,0 +1,12 @@
+class Piece:
+
+    def __init__(self) -> None:
+        pass
+
+    def move_a_piece(direcao):
+        horse = {
+            "up_right" : [+2,+1],
+            "up_left" : [+2,-1],
+            "up_left" : [+2,-1],
+            "up_left" : [+2,-1],
+        }
